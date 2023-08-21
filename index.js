ebfde19b056298ec03bd136aaf39a7a8bd85f20d
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2023-08-21 13:59:57 +0200

    Add function to copy password and for disappearing feedback

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ let password2 = document.getElementById("password2")
 let generateBtn = document.getElementById("btn")
 let copyBtn1 = document.getElementById("copy-btn1")
 let copyBtn2 = document.getElementById("copy-btn2")
+let feedback1 = document.getElementById("feedback1")
+let feedback2 = document.getElementById("feedback2")
 
 generateBtn.addEventListener("click", function() {
 
     let passwordLength = document.getElementById("characterLenght").value
     password1.innerText = ""
     password2.innerText = ""
+    feedback1.innerText = ""
+    feedback2.innerText = ""
 
     for (i = 0; i < passwordLength; i++) {
         let randomNumber1 = Math.floor(Math.random() * characters.length)
@@ -23,6 +27,30 @@ generateBtn.addEventListener("click", function() {
     }
 })
 
-copyBtn1.addEventListener("click", function() {
-    navigator.clipboard.writeText(password1.innerText)
-})
+    copyBtn1.addEventListener("click", function() {
+        if (password1.innerText != "") {
+            navigator.clipboard.writeText(password1.innerText)
+            feedback1.innerText = "Password copied to clipboard"
+
+            setTimeout(function() {
+                feedback1.innerText = ""
+            }, 3000);
+        } else {
+            feedback1.innerText = "Password field is empty!"
+        }
+    })
+
+    copyBtn2.addEventListener("click", function() {
+        if (password2.innerText != "") {
+            navigator.clipboard.writeText(password2.innerText)
+            feedback2.innerText = "Password copied to clipboard"
+
+            setTimeout(function() {
+                feedback2.innerText = ""
+            }, 3000);
+        } else {
+            feedback2.innerText = "Password field is empty!"
+        }
+    })
+
+
