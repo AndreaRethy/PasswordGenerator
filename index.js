1b118a51fefd83e7fb7136bc5b1a4dd40b1bff9c
Andrea <125359387+AndreaRethy@users.noreply.github.com>
2023-08-21 13:32:11 +0200

    create character length input field and copy buttons

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-let passwordLength = 15
-
 let password1 = document.getElementById("password1")
 let password2 = document.getElementById("password2")
-let btn = document.getElementById("btn")
+let generateBtn = document.getElementById("btn")
+let copyBtn1 = document.getElementById("copy-btn1")
+let copyBtn2 = document.getElementById("copy-btn2")
 
-btn.addEventListener("click", function() {
+generateBtn.addEventListener("click", function() {
+
+    let passwordLength = document.getElementById("characterLenght").value
+    password1.innerText = ""
+    password2.innerText = ""
+
     for (i = 0; i < passwordLength; i++) {
         let randomNumber1 = Math.floor(Math.random() * characters.length)
         let randomLetter1 = characters[randomNumber1]
@@ -16,4 +21,8 @@ btn.addEventListener("click", function() {
         let randomLetter2 = characters[randomNumber2]
         password2.innerText += randomLetter2
     }
+})
+
+copyBtn1.addEventListener("click", function() {
+    navigator.clipboard.writeText(password1.innerText)
 })
